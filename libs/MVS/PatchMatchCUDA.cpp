70312c41f8d1b34715fefff8752dfb120f6a7661
Piero Toffanin <pt@masseranolabs.com>
2022-09-20 10:26:22 -0400

    Ignore masks in outer scope

--- libs/MVS/PatchMatchCUDA.cpp
+++ libs/MVS/PatchMatchCUDA.cpp
@@ -408,14 +408,14 @@ void PatchMatch::EstimateDepthMap(DepthData& depthData)
 			lowResViewsMap = depthData.viewsMap;
 		}
 
-		// Apply ignore mask
-		if (OPTDENSE::nIgnoreMaskLabel >= 0) {
-			const DepthData::ViewData& view = depthData.GetView();
-
-			BitMatrix mask;
-			if (OPTDENSE::nIgnoreMaskLabel >= 0 && DepthEstimator::ImportIgnoreMask(*view.pImageData, depthData.depthMap.size(), mask, (uint16_t)OPTDENSE::nIgnoreMaskLabel))
-				depthData.ApplyIgnoreMask(mask);
-		}
+	}
+    
+	// apply ignore mask
+	if (OPTDENSE::nIgnoreMaskLabel >= 0) {
+		const DepthData::ViewData& view = depthData.GetView();
+		BitMatrix mask;
+		if (DepthEstimator::ImportIgnoreMask(*view.pImageData, depthData.depthMap.size(), mask, (uint16_t)OPTDENSE::nIgnoreMaskLabel))
+			depthData.ApplyIgnoreMask(mask);
 	}
 
 	// apply ignore mask
